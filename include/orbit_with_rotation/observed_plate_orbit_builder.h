904e0339be6db69dd1cd88c3b9038f63d79bf2cb
aimbotscv <cvtamurm@gmail.com>
2025-12-03 22:31:23 -0600

    bro vibed too hard. std::<math function> is not a thing

--- include/orbit_with_rotation/observed_plate_orbit_builder.h
+++ include/orbit_with_rotation/observed_plate_orbit_builder.h
@@ -17,8 +17,8 @@ class observed_plate_orbit_builder {
  public:
   PF_TARGET_ATTRS observed_plate_orbit from_one_plate(const observed_plate& plate_one) const noexcept {
     const float rotation = plate_one.rotation()[0];
-    const float sin_rot = std::sinf(rotation);
-    const float cos_rot = std::cosf(rotation);
+    const float sin_rot = sinf(rotation);
+    const float cos_rot = cosf(rotation);
 
     const Eigen::Vector3f radius_vector{radius_prior_ * sin_rot, radius_prior_ * cos_rot, 0.0f};
     const Eigen::Vector3f predicted_center = plate_one.position() + radius_vector;
@@ -33,8 +33,8 @@ class observed_plate_orbit_builder {
     const Eigen::Vector3f delta = 0.5f * (Eigen::Vector3f{} << -one_to_two[1], one_to_two[0], 0.0f).finished();
 
     const float rotation = plate_one.rotation()[0];
-    const float sin_rot = std::sinf(rotation);
-    const float cos_rot = std::cosf(rotation);
+    const float sin_rot = sinf(rotation);
+    const float cos_rot = cosf(rotation);
     const Eigen::Vector3f plate_normal{sin_rot, cos_rot, 0.0f};
     const float signed_side = plate_normal.dot(delta);
 
@@ -42,7 +42,7 @@ class observed_plate_orbit_builder {
     const Eigen::Vector3f mirrored_pos = midpoint - delta;
 
     const Eigen::Vector3f predicted_center =
-        (std::abs(signed_side) > 1e-5f)
+        (abs(signed_side) > 1e-5f)
             ? ((signed_side >= 0.0f) ? candidate_pos : mirrored_pos)
             : (((candidate_pos - observer_position_).squaredNorm() < (mirrored_pos - observer_position_).squaredNorm())
                    ? candidate_pos
